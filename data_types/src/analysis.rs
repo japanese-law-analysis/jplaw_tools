472c35941287ae5eaa6a3c8cf41f455264777782
puripuri2100 <puripuri2100@gmail.com>
2024-04-24 17:34:45 +0900

    解析に関する型を追加

---
+++ data_types/src/analysis.rs
@@ -0,0 +1,56 @@
+use crate::article::{ArticleIndex, TextIndex};
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+/// 解析結果を書き出すときの型
+#[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
+pub struct AnalysisResultInfo<T: std::fmt::Debug + Clone + std::hash::Hash + PartialEq + Eq> {
+  pub article_index: ArticleIndex,
+  pub text_index_opt: Option<TextIndex>,
+  pub result: T,
+}
+
+/// 解析結果を書き出すときの型
+#[derive(Debug, Error, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
+pub struct AnalysisError<
+  E: std::fmt::Debug + Clone + std::hash::Hash + PartialEq + Eq + std::error::Error,
+> {
+  pub article_index: ArticleIndex,
+  pub text_index_opt: Option<TextIndex>,
+  pub text: String,
+  pub error: E,
+}
+
+/// 読み替え規則の解析結果
+/// <https://github.com/japanese-law-analysis/analysis_yomikae>
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct YomikaeInfo {
+  /// 読み替えられる語
+  pub before_words: Vec<String>,
+  /// 読み替えられた後の単語
+  pub after_word: String,
+}
+
+/// 読み替え規則の解析の際のエラー
+/// <https://github.com/japanese-law-analysis/analysis_yomikae>
+#[derive(Debug, Error, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub enum YomikaeError {
+  #[error("Do not analysis table contents")]
+  ContentsOfTable,
+  #[error("Unmatched parentheses")]
+  UnmatchedParen,
+  #[error("Unexpected parallel words")]
+  UnexpectedParallelWords,
+  #[error("Not found yomikae sentence")]
+  NotFoundYomikae,
+}
+
+/// 略称解析の結果
+/// <https://github.com/japanese-law-analysis/analysis_ryakusyou>
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct Ryakusyou {
+  /// 略称
+  ryakusyou: String,
+  /// 正式名称
+  seishiki: String,
+}
--- data_types/src/lib.rs
+++ data_types/src/lib.rs
@@ -1,5 +1,6 @@
 //! よく使うデータ型を定義
 #![recursion_limit = "256"]
+pub mod analysis;
 pub mod article;
 pub mod jorei;
 pub mod law;
